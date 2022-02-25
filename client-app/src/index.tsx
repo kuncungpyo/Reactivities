a1feac1b11b569f09d99447606bd22bdf8725c8d
LAPTOP-GLMSM77M\tds <ib.dwianantaputra@btassurance.com>
2022-02-26 00:22:48 +0800

    End of section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,11 +5,14 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <StoreContext.Provider value={store}>
+  <StoreContext.Provider value={store}>
+    <BrowserRouter>
       <App />
-    </StoreContext.Provider>,
+    </BrowserRouter>
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 
